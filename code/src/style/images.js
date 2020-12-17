cc5ae168f5c45ec9b374fa2c6a1c519dfbd81ae1
Wesley Adriann <wesleyadriann@gmail.com>
2020-12-17 16:33:36 -0300

    Changes used icons
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- code/src/style/images.js
+++ code/src/style/images.js
@@ -1,9 +1,11 @@
 import searchIcon from '../assets/images/icons/search.png'
 import cancelIcon from '../assets/images/icons/cancel.png'
+import filterIcon from '../assets/images/icons/filter.png'
 
 export const IMAGES = {
   icons: {
     searchIcon,
-    cancelIcon
+    cancelIcon,
+    filterIcon
   }
 }
