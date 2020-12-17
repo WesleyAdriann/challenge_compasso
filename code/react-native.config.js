a7d46bed1ff80b47276ee4626baa8e463acbe6b3
Wesley Adriann <wesleyadriann@gmail.com>
2020-12-17 16:05:53 -0300

    Adds icons
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- code/react-native.config.js
+++ code/react-native.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   project: {
     ios: {},
-    android: {},
+    android: {}
   },
-  assets: ['./src/assets/fonts/Roboto'],
-};
+  assets: ['./src/assets/fonts/Roboto']
+}
---
+++ code/src/style/images.js
@@ -0,0 +1,9 @@
+import searchIcon from '../assets/images/icons/search.png'
+import cancelIcon from '../assets/images/icons/cancel.png'
+
+export const IMAGES = {
+  icons: {
+    searchIcon,
+    cancelIcon
+  }
+}
